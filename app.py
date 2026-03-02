f64c1731ecf8b9390745269ec05e265b75ca9459
NickHullEmerson <EMERSON.ROSAALVES@GMAIL.COM>
2026-03-02 09:20:52 -0300

    app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.markdown("""
     .main { background-color: #0e1117; }
     .block-container {
         padding-top: 1rem !important; 
-        margin-top: -15px !important;
+        margin-top: -10px !important;
     }
     
    .header-text { 
